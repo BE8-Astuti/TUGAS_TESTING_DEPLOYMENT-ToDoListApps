7240d95d652c7b4716f26ae6624337b421cd51ae
Astuti <100201094+astutirahmawati@users.noreply.github.com>
2022-05-21 13:55:32 +0700

    Update interface.go

--- repository/user/interface.go
+++ repository/user/interface.go
@@ -4,5 +4,5 @@ import "projek/be8/entities"
 
 type User interface {
 	InsertUser(newUser entities.User) (entities.User, error)
-	Login(name string, password string) (entities.User, error)
+	Login(email string, password string) (entities.User, error)
 }
