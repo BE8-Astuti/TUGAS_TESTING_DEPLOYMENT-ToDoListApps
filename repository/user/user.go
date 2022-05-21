93e2bab0922544f60e34de5c9025bc4147e630b4
Astuti <100201094+astutirahmawati@users.noreply.github.com>
2022-05-21 13:55:07 +0700

    Update user.go

--- repository/user/user.go
+++ repository/user/user.go
@@ -29,10 +29,10 @@ func (ur *UserRepo) InsertUser(newUser entities.User) (entities.User, error) {
 	return newUser, nil
 }
 
-func (ur *UserRepo) Login(name string, password string) (entities.User, error) {
+func (ur *UserRepo) Login(email string, password string) (entities.User, error) {
 	users := []entities.User{}
 
-	if err := ur.Db.Where("name = ? AND password = ?", name, password).First(&users).Error; err != nil {
+	if err := ur.Db.Where("email = ? AND password = ?", email, password).First(&users).Error; err != nil {
 		log.Warn(err)
 		return entities.User{}, errors.New("tidak bisa select data")
 	}
